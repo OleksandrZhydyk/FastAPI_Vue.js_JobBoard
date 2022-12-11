5a864feb6848589e0a5f93a4dbf4f9bfb602c62a
AlexandrZhydyk <zhydykalex@ukr.net>
2022-12-11 21:18:24 +0200

    created venv, tables, docker, docker compose configurations

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+
+from src.db.base import database
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+if __name__ == "__main__":
+    uvicorn.run()
