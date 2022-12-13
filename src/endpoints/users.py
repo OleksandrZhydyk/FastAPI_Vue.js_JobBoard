8ab335618a4a75689390b5a26c06a068f50c13d1
AlexandrZhydyk <zhydykalex@ukr.net>
2022-12-13 22:06:19 +0200

    libs added

--- src/endpoints/users.py
+++ src/endpoints/users.py
@@ -1,8 +1,12 @@
+from typing import List
+
 from fastapi import APIRouter
+
+from models.user import User
 
 router = APIRouter
 
 
-@router.get("/")
+@router.get("/", response_model=List[User])
 async def read_users(limit: int = 100, skip: int = 100):
     return {}
