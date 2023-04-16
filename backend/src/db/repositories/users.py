9ab683ef8305f92fdbf5d5a48a165ff2fde1b12f
AlexandrZhydyk <zhydykalex@ukr.net>
2023-04-16 22:30:07 +0300

    ci try fix 2

--- backend/src/db/repositories/users.py
+++ backend/src/db/repositories/users.py
@@ -89,7 +89,7 @@ class UsersService:
             )
         # print(db_obj.scalar_one().__dict__)
         a = db_obj.scalar_one()
-        print(a.email)
+        print(a)
         return a
 
     async def get_all(self, db: AsyncSession) -> List[UserOut]:
